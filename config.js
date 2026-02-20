9809599625dcd315f13cee631ba5a4c4e9861e31
Robert <krstb-90@yahoo.com>
2026-02-20 23:14:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Diese Datei darf NIEMALS auf GitHub hochgeladen werden!
 const CONFIG = {
-    apiKey: "AIzaSyAojD86DWlm-zGArbS-PXNBSoF3FmfwRRkx",
+    apiKey: "122333333zGArbS-PXNBSoF3FmfwRRk",
     CLIENT_ID: "DEINE_CLIENT_ID_FALLS_VORHANDEN"
 };
