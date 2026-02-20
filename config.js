0ce0062c9300c6c9c056088d7090aaca27186243
Robert <krstb-90@yahoo.com>
2026-02-20 16:38:57 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Diese Datei darf NIEMALS auf GitHub hochgeladen werden!
+const CONFIG = {
+    API_KEY: 'DEIN_NEUER_REGENERIERTER_SCHLÜSSEL_HIER',
+    CLIENT_ID: 'DEINE_CLIENT_ID_FALLS_VORHANDEN'
+};
