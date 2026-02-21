b2f5524281d1d17cba7d3045b4cfa227246d8101
Robert <krstb-90@yahoo.com>
2026-02-21 01:17:27 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Diese Datei darf NIEMALS auf GitHub hochgeladen werden!
-const CONFIG = {
-    apiKey: "AIzaSyAojD86DWlm-zGArbS-PXNBSoF3FmfwRRk",
-    CLIENT_ID: "DEINE_CLIENT_ID_FALLS_VORHANDEN"
-};
